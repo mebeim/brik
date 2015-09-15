5ee68a244f4c6e550fa96cced2bd00b59fb75eea
Marco Bonelli <mebeim@users.noreply.github.com>
2015-09-15 15:35:00 +0200

    All fixed, substancial improvements in hitbox detection

--- src/js/game.js
+++ src/js/game.js
@@ -1,12 +1,12 @@
 Object.defineProperties(Number.prototype, {
-	'between': {
+	between: {
 		value: function(a, b) {
 			if (this >= a && this <= b) return true;
 			return false;
 		}
 	},
 	
-	'outside': {
+	outside: {
 		value: function(a, b) {
 			if (this <= a || this >= b) return true;
 			return false;
@@ -15,9 +15,23 @@ Object.defineProperties(Number.prototype, {
 });
 
 Object.defineProperties(Math, {
-	'atanP' : {
+	atanP : {
 		value: function(m) {
 			return (m < 0 ? this.PI : 0) + this.atan(m);
+		}
+	}
+});
+
+Object.defineProperties(CanvasRenderingContext2D.prototype, {
+	fastLine: {
+		value: function(x1, y1, x2, y2, color, width) {
+			this.beginPath();
+			if (color) this.strokeStyle = color;
+			if (width) this.lineWidth = width;
+			this.moveTo(x1, y1);
+			this.lineTo(x2, y2);
+			this.closePath();
+			this.stroke();
 		}
 	}
 });
@@ -42,16 +56,6 @@ function Game() {
 			dark_green	: [/*'hsl(120, 100%, 5%)',		'hsl(120, 100%, 10%)',*/	'hsl(120, 100%, 15%)'	],
 		},
 		animationID, pad, c;
-		
-	function fastLine(x1, y1, x2, y2, color, width) {
-		c.beginPath();
-		c.strokeStyle = color;
-		c.lineWidth = width;
-		c.moveTo(x1, y1);
-		c.lineTo(x2, y2);
-		c.closePath();
-		c.stroke();
-	}
 	
 	function Brick(x, y, w, h, color, health) {	
 		var borderW = w/15,
@@ -69,31 +73,60 @@ function Game() {
 			
 			c.fillRect(x, y, w, h);
 			c.strokeRect(x+borderW/2, y+borderW/2, w-borderW, h-borderW);
+			
+			// DEBUG 
+			// Ball hitbox on the brick (to be done with only 1 brick)
+			var dX, dY, R = 1.5/100*gameW;
+			for (var i=x-R; i<x2+R; i++) {
+				for (var j=y-R; j<y2+R; j++) {
+					dX = Math.abs(i - x - w/2) - w/2;
+					dY = Math.abs(j - y - h/2) - h/2;
+					
+					dX = dX >= 0 ? dX : 0;
+					dY = dY >= 0 ? dY : 0;
+					
+					if (dX*dX + dY*dY <= R*R && (dX || dY)) { 
+						if (dX && dY) c.fillStyle = 'rgba(0, 0, 0, 1)';
+						else c.fillStyle = 'rgba(0, 0, 0, 0.5)';
+						c.fillRect(i, j, 1, 1);
+					}					
+				}
+			} 
 		}
 		
-		this.collision = function(ballX, ballY, R, ballTeta) {
-			var dX = Math.abs(ballX - x - w/2) - w/2,
-				dY = Math.abs(ballY - y - w/2) - h/2,
-				dX2 = dX*dX,
-				dY2 = dY*dY,
-				R2 = R*R,
-				vX, vY;
-				
-			vX = Math.abs(x-ballX) < Math.abs(x2-ballX) ? x : x2;
-			vY = Math.abs(y-ballY) < Math.abs(y2-ballY) ? y : y2;
-			fastLine(vX, vY, ballX, ballY, 'black', 2);
+		this.collision = function(ballX, ballY, ballR, ballTeta) {
 			
-			if (dX <= R && dY <= R) {
-				void(0);
+			/*
+			 * This function returns true if the ball hits the brick, false otherwise.
+			 * It also decreases the brick's health until 0, when the brick dies.
+			 *
+			 * this.newTeta: new angle of the ball after the collision
+			 * dX: distance from the center of the ball and the nearest vertical border
+			 * dY: distance from the center of the ball and the nearest horizontal border
+			 * vX: x of the nearest vertex
+			 * vY: y of the nearest vertex
+			 *
+			 */
+			
+			var dX = Math.abs(ballX - x - w/2) - w/2,
+				dY = Math.abs(ballY - y - h/2) - h/2,
+				R2 = ballR*ballR,
+				vX, vY, dX2, dY2;
 				
-				if (dX <= R /*ballX.between(x-ballR, x2+ballR) && ballY.between(y, y2)*/)			// Vertical borders
-					this.newTeta = ballTeta <= pi ? pi - ballTeta : 3*pi - ballTeta;
-				else if (dY <= R /*ballY.between(y-ballR, y2+ballR) && ballX.between(x, x2)*/)	// Horizontal borders
-					this.newTeta =  2*pi - ballTeta;
-				else if (dX2 + dY2 <= R2) {															// Vertexes
+			dX2 = (dX < 0) ? 0 : dX*dX;
+			dY2 = (dY < 0) ? 0 : dY*dY;
+			
+			// If collision
+			if (dX2 + dY2 <= R2) {				
+				// Check collision for vertical borders, horizontal borders and vertexes
+				if (!dY2) this.newTeta = ballTeta <= pi ? pi - ballTeta : 3*pi - ballTeta;
+				else if (!dX2) this.newTeta =  2*pi - ballTeta;		
+				else {
 					// Find which vertex is involved
+					vX = Math.abs(x-ballX) < Math.abs(x2-ballX) ? x : x2;
+					vY = Math.abs(y-ballY) < Math.abs(y2-ballY) ? y : y2;
 					
-					// Reflect
+					// To do: this still generates negative angles
 					this.newTeta = 2*Math.atanP((vY-ballY)/(vX-ballX)) - ballTeta - pi;
 				}
 				
@@ -104,21 +137,17 @@ function Game() {
 	}
 	
 	function Pad() {
-		var r = 7/100*gameW,
-			h = 1/100*gameH,
+		var r = 7e-2*gameW,
+			h = 1e-2*gameH,
 			x = gameW/2,
 			y = gameH-h;
 	
 		function draw() {
 			c.fillStyle = 'white';
 			c.fillRect(x-r, y, 2*r, h);
-			
-			// DEBUG
-			fastLine(x, y, x, 0, 'white', 2);
 		}
-		
-		// To do: make speeds relative to canvas size
-		this.speed = 15;
+
+		this.speed = 1.5e-2*gameH;
 		this.direction = false;
 		this.newTeta = 0;
 		
@@ -158,13 +187,12 @@ function Game() {
 	}
 	
 	function Ball() {
-		var r = 1.5/100*gameW,
+		var r = 1.5e-2*gameW,
 			x = gameW/2,
 			y = gameH - gameH/100*5,
 			teta = Math.random()*4*pi/6 + pi/6;
-		
-		// To do: make speeds relative to canvas size
-		this.speed = 10;
+
+		this.speed = 1e-2*gameH;
 			
 		function draw() {
 			c.fillStyle = 'lightgrey';
@@ -174,10 +202,12 @@ function Game() {
 			c.fill();
 			
 			/* DEBUG
-			fastLine(x, 0, x, gameH, 'lightgrey', 2);
-			fastLine(0, y, gameW, y, 'lightgrey', 2);*/
-			c.fillText('ballTeta = ' + teta/pi*180, 50, gameH - 200); 
-			fastLine(x, y, x + 1000*Math.cos(-teta), y + 1000*Math.sin(-teta), 'pink', 2);
+			c.fastLine(x, 0, x, gameH, 'lightgrey', 2);
+			c.fastLine(0, y, gameW, y, 'lightgrey', 2);*/
+			c.fillText('## DEBUG MODE ##', 50, gameH - 200);
+			c.fillText('θ = ' + (teta/pi*180).toFixed(3) + '°', 50, gameH - 180);
+			c.fillText('m = ' + Math.tan(teta).toFixed(3), 50, gameH - 170);
+			c.fastLine(x, y, x + 1000*Math.cos(-teta), y + 1000*Math.sin(-teta), 'pink', 2);
 		}
 			
 		this.update = function() {
@@ -220,8 +250,9 @@ function Game() {
 		}
 		*/
 		
-		// Only one brick:
-		b = new Brick(3*width, 4*height, width, height, 'red', 3);
+		// DEBUG
+		// Spawn only one red brick
+		b = new Brick(4*width, 7*height, width, height, 'red', 3);
 		b.draw();
 		bricks.push(b);
 	}
@@ -229,9 +260,9 @@ function Game() {
 	function update() {
 		c.clearRect(0, 0, gameW, gameH);
 		
-		for (var i=0, b; b = bricks[i]; i++) b.draw();
 		pad.update();
 		ball.update();
+		for (var i=0, b; b = bricks[i]; i++) b.draw();
 		
 		animationID = requestAnimationFrame(update);
 	}
